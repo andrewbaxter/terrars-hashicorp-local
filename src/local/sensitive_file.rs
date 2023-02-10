73c8c01010a08c19014b8768ac61acc9a9148423
andrew <>
2023-02-10 11:17:37 +0900

    Version updates

--- src/local/sensitive_file.rs
+++ src/local/sensitive_file.rs
@@ -41,7 +41,7 @@ impl SensitiveFile {
         &self.0.shared
     }
 
-    pub fn depends_on(self, dep: &impl Dependable) -> Self {
+    pub fn depends_on(self, dep: &impl Referable) -> Self {
         self.0.data.borrow_mut().depends_on.push(dep.extract_ref());
         self
     }
@@ -163,24 +163,20 @@ impl SensitiveFile {
     }
 }
 
-impl Resource for SensitiveFile {
+impl Referable for SensitiveFile {
     fn extract_ref(&self) -> String {
         format!("{}.{}", self.0.extract_resource_type(), self.0.extract_tf_id())
     }
 }
 
-impl Dependable for SensitiveFile {
-    fn extract_ref(&self) -> String {
-        Resource::extract_ref(self)
-    }
-}
+impl Resource for SensitiveFile { }
 
 impl ToListMappable for SensitiveFile {
     type O = ListRef<SensitiveFileRef>;
 
     fn do_map(self, base: String) -> Self::O {
         self.0.data.borrow_mut().for_each = Some(format!("${{{}}}", base));
-        ListRef::new(self.0.shared.clone(), Resource::extract_ref(&self))
+        ListRef::new(self.0.shared.clone(), self.extract_ref())
     }
 }
 
